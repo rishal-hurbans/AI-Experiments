2443cd0c4f0675dc301e351718fe7ca3d2377ecc
Rishal Hurbans <rishal.hurbans@gmail.com>
2016-05-22 17:57:53 +0200

    Added password strength app. Added comments to hello world, and iris examples.

--- Machine Learning/2-iris_vis.py
+++ Machine Learning/2-iris_vis.py
@@ -1,25 +1,27 @@
-import numpy as np
 from sklearn.datasets import load_iris
 from sklearn import tree
+import numpy as np
 
 iris = load_iris()
 test_idx = [0, 50, 100]
 
-#training data
+#Training data
 training_target = np.delete(iris.target, test_idx)
 training_data = np.delete(iris.data, test_idx, axis = 0)
 
-#testing data
+#Testing data
 testing_target = iris.target[test_idx]
 testing_data = iris.data[test_idx]
 
+#The classifier
 clf = tree.DecisionTreeClassifier()
+#Training the classifier
 clf.fit(training_data, training_target)
 
 print testing_target
 print clf.predict(testing_data)
 
-#vis code
+#Visualising the process as a tree
 from sklearn.externals.six import StringIO
 import pydot
 dot_data = StringIO()  
